4367909a4657c2f91ae8435e3e80181ae46f011b
Anthony Maddatu <amaddatu@gmail.com>
2019-05-02 21:15:32 -0400

    Add local login

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import AUTHAPI from './utils/google-auth';
 import PersonalAccount from './components/personal-account';
 import PrivacyPolicy from './components/privacy-policy';
+import LoginLocal from './components/login-local';
 
 class App extends Component {
   state = {
@@ -22,6 +23,11 @@ class App extends Component {
       <div className="App">
         <Router>
           <Route exact path="/privacy-policy" component={PrivacyPolicy} />
+          <Route exact path="/login-local" render={
+            () => (
+              <LoginLocal {...this.props} setUser={this.setUser} user={this.state.user} />
+            )
+          } />
           <Route exact path="/personal-account" render={
             () => (
               <PersonalAccount {...this.props} setUser={this.setUser} user={this.state.user} />
@@ -37,6 +43,14 @@ class App extends Component {
                 <p>
                   Edit <code>src/App.js</code> and save to reload.
                 </p>
+                
+                <Link
+                  className="App-link"
+                  to="/login-local"
+                  rel="noopener noreferrer"
+                >
+                  Local Login
+                </Link>
                 <a
                   className="App-link"
                   href={AUTHAPI.getAuthorizeLink()}
